009c70684e9a93513e696bf056f0d5d2e6b28f17
Antonio Idelvane <idel.the@gmail.com>
2021-10-11 22:10:27 -0300

    arquivo de configuracao do TravisCI e refatoracao do pacote de teste

--- src/test/java/io/github/idelvane/customermanagement/test/BuildersCustomersApplicationTests.java
+++ src/test/java/io/github/idelvane/customermanagement/test/BuildersCustomersApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.managecustomers;
+package io.github.idelvane.customermanagement.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
